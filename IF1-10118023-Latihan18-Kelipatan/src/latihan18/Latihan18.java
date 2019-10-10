005ceca96b95e4f77282275b3774d451346c2811
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-10-10 14:01:56 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan18-Kelipatan/src/latihan18/Latihan18.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan18;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Kelipatan
+ */
+
+public class Latihan18 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++)
+            System.out.println(3.5*i);
+        
+        System.out.println("");
+        System.out.println("Developed by : Zaenal Anzarry");
+        
+    }
+    
+}
